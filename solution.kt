a760d406f311bfa1187c349b1d62aa94e66432c7
Candice Han <jeeyoung.han82@gmail.com>
2021-03-28 21:07:48 +0900

    Finish till Power

--- solution.kt
+++ solution.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 private object Solution1 {
 
     fun countUniqueValues(list: List<Int>): Int {
@@ -14,17 +16,51 @@ private object Solution1 {
     fun printNumber(n: Int): List<Int> {
         return (n downTo 1).toList()
     }
+
+    fun printNumberWithStep(n: Int, step: Int = 1): List<Int> =
+            (n downTo 1 step step).toList()
+
+    fun power(base: Int, exponent: Int): Int {
+        if (exponent == 1) {
+            return base
+        }
+        return base * power(base, exponent - 1)
+    }
 }
+
 
 private object Solution2 {
     fun countUniqueValues(list: List<Int>): Int {
         return list.toSet().size
+    }
+
+    fun printNumberWithStep(n: Int, step: Int = 1): List<Int> {
+        fun printNumberRec(n: Int): List<Int> =
+                when {
+                    n <= 0 -> emptyList()
+                    else -> listOf(n) + printNumberRec(n - step)
+                }
+        return printNumberRec(n)
+    }
+
+    fun power(base: Int, exponent: Int): Int {
+        return base.toDouble().pow(exponent).toInt()
     }
 }
 
 private object Solution3 {
     fun countUniqueValues(list: List<Int>): Int {
         return list.distinct().size
+    }
+
+    private fun printNumberWithStep(n: Int, step: Int = 1): List<Int> {
+        val list = mutableListOf<Int>()
+
+        if (n <= 0) return emptyList()
+        else list.add(n)
+
+        list.addAll(printNumberWithStep(n - step, step))
+        return list
     }
 }
 
